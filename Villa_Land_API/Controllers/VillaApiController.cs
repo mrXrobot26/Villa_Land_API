5e594d5c27204aa7b29f03f03e37ecfd1b9d42a1
Abdo Elbessa <zezoelbessa10@egyeagles.com>
2023-10-26 16:48:52 +0300

    DTO

---
+++ Villa_Land_API/Controllers/VillaApiController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VillaLand.Models;
+using VillaLand.Models.DTO;
+
+namespace Villa_Land_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaApiController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return new List<VillaDTO>{
+                new VillaDTO { Id =1, Name ="bool view"},
+                new VillaDTO { Id =2,Name="Beach View"}
+            };
+
+        }
+    }
+}
